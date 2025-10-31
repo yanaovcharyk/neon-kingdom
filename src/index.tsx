92f4fd0fd09e72f53f1f48d449c4b369937cd11f
Yana Ovcharyk <yanaovcharik@gmail.com>
2025-10-31 18:10:42 +0200

    fix: add basename in BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import "./styles/global.scss";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/yanaovcharyk">
+    <BrowserRouter basename="/neon-kingdom">
       <App />
     </BrowserRouter>
   </React.StrictMode>
